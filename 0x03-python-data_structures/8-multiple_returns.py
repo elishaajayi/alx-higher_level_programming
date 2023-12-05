005db93bf555f07573e16aafec2372b9ef731eb6
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-05 05:28:12 +0100

    Fix: Task 8 and 12

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first = sentence[0]
 
-    if length == 0:
-        return (length, None)
+    if length > 0:
+        first = sentence[0]
     else:
-        return (length, first)
+        first = None
+
+    return (length, first)
