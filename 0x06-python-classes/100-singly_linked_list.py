c55f5eef88c9bccefcebf5fd771c361b12c5849a
elishaajayi <ajayienterprises.dev@gmail.com>
2023-12-20 02:13:25 +0100

    Fix: Hopefully requirement is fulfilled

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""Module containing the Node and SinglyLinkedList class."""
-
-
 class Node:
-    """The node class designed for queue, singlylinked list, stack."""
-
     def __init__(self, data, next_node=None):
-        """Initialization of the node.
-        Args:
-            data (int): The integer value to be stored in the node.
-            next_node (:obj:'Node'): The next node.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """int: Integer value stored in the node."""
         return self.__data
 
     @data.setter
@@ -28,7 +17,6 @@ class Node:
 
     @property
     def next_node(self):
-        """:obj:'Node': The next node."""
         return self.__next_node
 
     @next_node.setter
@@ -40,18 +28,10 @@ class Node:
 
 
 class SinglyLinkedList:
-    """The singlylinked list class"""
-
     def __init__(self):
-        """Initialization of the singly linked list."""
         self.__head = None
 
     def sorted_insert(self, value):
-        """Inserts a node in increasing order of the singly linked list.
-        Args:
-            value (int): The integer value of the node to be inserted into the
-                singly linked list.
-        """
         curr_n = self.__head
         if curr_n is None or value < curr_n.data:
             self.__head = Node(value, curr_n)
@@ -62,11 +42,6 @@ class SinglyLinkedList:
             curr_n.next_node = Node(value, curr_n.next_node)
 
     def __str__(self):
-        """Returns a string containing the integer values of each node on
-        separate lines
-        Returns:
-            str: The string of integers values
-        """
         string = ""
         if self.__head is not None:
             string = str(self.__head.data)
--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-"""Module containing the Square class"""
-
-
 class Square:
-    """The Square class"""
     def __init__(self, size=0, position=(0, 0)):
-        """Initializing an instance of Square
-        Args:
-            size (int): The size of the Square instance. Default value is 0.
-            position (:obj:'tuple' of int): x, y coordinate offset when
-                printing the square.
-        """
         self.size = size
         self.position = position
 
     def __str__(self):
-        """Returns a string consisting of a square made with hashtags using the
-        'size'. Uses 'position' to offset where the printing should begin. The
-        x, y coordinate in 'position' is the location of the top left corner of
-        the square.
-        Returns:
-            str: A string representing a square made of hashtags.
-        """
         __my_string = ""
-        if self.__size is not 0:
+        if self.__size != 0:
             for y in range(self.__position[1]):
                 __my_string += '\n'
             for i in range(self.__size):
@@ -51,13 +34,11 @@ class Square:
 
     @property
     def position(self):
-        """:obj:'tuple' of int: x, y coordinate offset when printing the square.
-        """
         return self.__position
 
     @position.setter
     def position(self, position):
-        if (not isinstance(position, tuple) or len(position) is not 2 or
+        if (not isinstance(position, tuple) or len(position) != 2 or
             not isinstance(position[0], int) or
                 not isinstance(position[1], int) or position[0] < 0 or
                 position[1] < 0):
@@ -66,17 +47,10 @@ class Square:
         self.__position = position
 
     def area(self):
-        """Returns the current square area of the instance
-        Returns:
-            int: Value of 'size'
-        """
         return self.__size ** 2
 
     def my_print(self):
-        """Prints a square with hashtags using the 'size'. Uses 'position' to
-        offset where the printing should begin. The x, y coordinate in
-        'position' is the location of the top left corner of the square."""
-        if self.__size is not 0:
+        if self.__size != 0:
             for y in range(self.__position[1]):
                 print()
             for i in range(self.__size):
